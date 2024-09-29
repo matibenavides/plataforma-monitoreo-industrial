dd46da3dfd4d1efa8fce338b5e565ec78cd08f2b
Matias Benavides <matias.benavides.rojas@gmail.com>
2024-09-28 21:37:20 -0300

    Creación de estructura bd en archivo models.py, más migraciones a mysql, todo oki (consideran cambios a futuro)

--- ddc/settings.py
+++ ddc/settings.py
@@ -80,14 +80,16 @@ WSGI_APPLICATION = 'ddc.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        # 'NAME' : '',
-        # 'USER' : '',
-        # 'PASSWORD' : ''
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME' : '',
+        'USER' : '',
+        'PASSWORD' : '',
+        'HOST': '',
+        'PORT': ''
     }
 }
 
+# 'NAME': BASE_DIR / 'db.sqlite3',
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
