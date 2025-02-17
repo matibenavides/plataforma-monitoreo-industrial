12e6e166c22c377c048c549b6be2a54b3cb8861c
Matias Benavides <matias.benavides.rojas@gmail.com>
2025-02-17 17:26:04 -0300

    feat(templates): Se agrega template listatemperatura.html y se ajustan redirecciones
    - Se cambia botón 'Listado' de temperatura.html con mejor formato (etiqueta)
    - tablatemperatura.html ahora recibe campo hidden desde backend (función: mostrarTemperatura)
        - Este campo es para recibir el id de la Línea el cual se trabaja, mediante el navbar
    - urls.py: modifica path de mostrarTemperatura, para recibir id (id lineas)
        - Se agrega nueva ruta ('archivos_temperatura/', views.mostrarListaTemperatura) para llamar función
    - views.py: Se importa 'Paginator' y 'Q' de busqueda, para listar y buscar elementos
        - 'registrarTemperatura': es editado el codigo para recibir el campo de linea_id y recibe trabajador logeado para registrarlo
        - 'mostrarListaTemperatura': Se crea está función para mostrar los datos agrupados en GrupoCloración usando 'Paginator' y buscador 'Q'
            - Recibe los datos de GrupoCloración, genera busqueda, formatea los datos y los presenta en un diccionario
    
    - Se crea archivo listatemperatura.html que tiene el mismo formato de listaonce.html de la App Cloracion.
        - Su diferencia recae en las redirecciones y los datos a recibir.
        - Utiliza misma interfaz visual (css) y funciones javaScripts, para mostrar Toast, tooltip y modal
        - Los includes que usan el Paginator de la app Cloraciones, también funciona en este template.
    - Queda en ajustar ciertos parametros de modals, que toman relacion con App Cloraciones. Para ser usados en App Temperaturas
    
    - (App/Cloraciones):
        - cloraciones/js/localstorage.js : Solo cambia mensaje a uno más general, para ser reutilizado en otras Apps
        - cloraciones/base/listaonce.html: Ajuste de código visual y etiqueta
        - cloraciones/base/navbar.html: Agrega los urls a los hipervínculos para mandar el linea_id al path
            - (<a href="{% url 'temperatura' linea_id=1 %})

--- Temperaturas/urls.py
+++ Temperaturas/urls.py
@@ -3,8 +3,11 @@ from . import views
 
 urlpatterns = [
     #----- Vista hacia plantilla Temperatura -----#
-    path('temperatura/', views.mostrarTemperatura),
+    path('temperatura/<int:linea_id>', views.mostrarTemperatura, name="temperatura"),
 
     #----- Vista que guarda los registros de 'temperatura/' -----#
     path('guarda_temperatura', views.registrarTemperatura),
+
+     #----- Vista hacia Listado de registros -----#
+    path('archivos_temperatura/', views.mostrarListaTemperatura, name="listatemperatura"),
 ]
