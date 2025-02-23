39bded75a387d3395c8df20b943f25211ad9ec67
Matias Benavides <matias.benavides.rojas@gmail.com>
2025-02-23 15:35:11 -0300

    feat(views): Función para registrar datos de PPM
    - views.py : Se crea 'registrarPPM', toma valores, ph a flotante, guarda en bd y manda mensaje
    - urls.py : Agrega ruta al path 'registrarppm'
    - ppm.html : Ajuste de codigo, el formulario no puede enviar contenido en campos disabled
        - Se cambia para que sea un campo readonly y reciba el id que se envio desde navbar

--- PPM/views.py
+++ PPM/views.py
@@ -22,3 +22,46 @@ def mostrarPPM(request, linea_id):
         'linea': linea,
     } 
     return render(request, "ppms/base/ppm.html", datos)
+
+@login_required(login_url='inicio')
+def registrarPPM(request):
+    if request.method == 'POST':
+
+        
+
+        linea = Lineas.objects.get(id=request.POST['lineaop'])
+        turno = Turnos.objects.get(id=request.POST['turnoop'])
+        trabajador = request.user.trabajador
+        hora = request.POST['hora']
+
+        fecha = request.POST['fecha']
+        dia_obj, created = Dia.objects.get_or_create(dia_dia=fecha)
+        #Solución, 'Cannot assign "'2025-02-23'": "PPM.dia_id" must be a "Dia" instance.'
+
+        ppm = request.POST['ppm']
+        phstr = request.POST['ph']
+
+        ph = float(phstr) if phstr else None
+        observacion = request.POST['observacion']
+
+        ppm = PPM.objects.create(
+            lineas_id=linea,
+            trabajador_id=trabajador,
+            turnos_id=turno,
+            dia_id=dia_obj, 
+            hor_ppm=hora,
+            dat_ppm=ppm,
+            phe_ppm=ph,
+            obs_ppm=observacion,
+        )
+
+        datos = {
+            'msg': '¡Registro agregado correctamente!',
+            'sector' : 'PPM',
+        }
+    
+    return render(request, 'ppms/base/ppm.html', datos)
+
+    
+
+
