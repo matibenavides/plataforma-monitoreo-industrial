9252ecb6268ee9cdcf974ff05c3907f77cb75578
Matias Benavides <matias.benavides.rojas@gmail.com>
2025-03-04 18:07:06 -0300

    feat(views): Funcionaliad de listado de datos (ruta navbar / Reportes), más busqueda dinamica
    - Se crean dos funciones 'mostrarListaFungicida' y 'eliminarlistaFungicida'
        -lista : Enseña todos los registros, más un buscador dinamico
            - Filtra el tipo de datos si quieres buscar datos similares por:
            - Fecha, Hora, Producto, Peso Inicial, CC Producto, Linea, Especie, Variedad, Dilución por agua/cera y Trabajador
        -eliminacion: Esta función es solo para poder eliminar registros dentro del template listafungicida.html
        - Modal propio para tomar la ruta independiente y hacer redirect a 'listafungicida' en vez de 'fungicida'

--- fungicidas/views.py
+++ fungicidas/views.py
@@ -240,3 +240,135 @@ def eliminarFungicida(request, grupo_id):
         messages.error(request, f'¡Error, registro inexistente! {e}')
         return redirect('fungicida', linea_id=linea.id)
     
+@login_required(login_url='inicio')
+def eliminarlistaFungicida(request, grupo_id):
+    try:
+        dosificacion = Dosificacion.objects.get(id=grupo_id)
+        dosificacion.delete()
+        messages.success(request, '¡Registro eliminado correctamente!')
+        return redirect('listafungicida')
+    except Exception as e:
+        messages.error(request, f'¡Error, registro inexistente! {e}')
+        return redirect('listafungicida')
+    
+
+@login_required(login_url='inicio')
+def mostrarListaFungicida(request):
+
+    busqueda = request.GET.get("buscar")
+    campo = request.GET.get("campo")
+    lista = Dosificacion.objects.all().order_by('-id')
+    
+    if campo:
+        if campo == "fecha":
+            try:
+                fecha = busqueda.split('-')
+                if len(fecha) != 3:
+                    messages.error(request, '¡El formato de fecha debe ser YYYY-MM-DD!')
+                    return redirect('listafingicida')
+                
+                lista = Dosificacion.objects.filter(dia_id__dia_dia__exact=busqueda)
+            except:
+                messages.error(request, '¡El formato de fecha debe ser YYYY-MM-DD!')
+        elif campo == "hora":
+            try:
+                hora = busqueda.split(':')
+                if len(hora) !=2:
+                    messages.error(request, '¡El formato de hora debe ser HH:MM!')
+                    return redirect('listafungicida')
+                
+                lista = Dosificacion.objects.filter(hor_dos__exact=busqueda)
+            except:
+                messages.error(request, '¡El formato de hora debe ser HH:MM!')
+        elif campo == "producto":
+            if busqueda.replace('.','',1).isdigit():
+                messages.error(request, '¡La busqueda no puede iniciar con un número o signos')
+                return redirect('listafungicida')
+            lista = Dosificacion.objects.filter(Q(fungicidas_id__nom_fun__icontains=busqueda)).distinct()
+        elif campo == "peso_inicial":
+            if not busqueda.isdigit():
+                messages.error(request, '¡El valor de peso, debe ser un número entero!')
+                return redirect('listafungicida')
+            lista = Dosificacion.objects.filter(pei_dos__exact=busqueda)
+        elif campo == "cc_producto":
+            if not busqueda.isdigit():
+                messages.error(request, '!El valor de cc, debe ser un número entero!')
+                return redirect('listafungicida')
+            lista = Dosificacion.objects.filter(ccp_dos__exact=busqueda)
+        elif campo == "linea":
+            try:
+                linea_num = int(busqueda)
+                if linea_num not in [1, 10]:
+                    messages.error(request, '¡La línea debe ser 1 o 10')
+                    return redirect('listafungicida')
+                lista = Dosificacion.objects.filter(lineas_id__num_lin__exact=busqueda)
+            except ValueError:
+                messages.error(request, '¡El valor de línea debe ser un número!')
+                return redirect('listafungicida')
+        elif campo == "especie":
+            if busqueda.replace('.','',1).isdigit():
+                messages.error(request, '¡La busqueda no puede iniciar con un número o signos')
+                return redirect('listafungicida')
+            lista = Dosificacion.objects.filter(Q(especies_id__nom_esp__icontains=busqueda)).distinct()
+        elif campo == "variedad":
+            if busqueda.replace('.','',1).isdigit():
+                messages.error(request, '¡La busqueda no puede iniciar con un número o signos')
+                return redirect('listafungicida')
+            lista = Dosificacion.objects.filter(Q(variedad_id__nom_var__icontains=busqueda)).distinct()
+        elif campo == "agua":
+            if not busqueda.isdigit():
+                messages.error(request, '¡El valor de Dilución en Agua, debe ser un número entero!')
+                return redirect('listafungicida')
+            lista = Dosificacion.objects.filter(agu_dos__exact=busqueda)
+        elif campo == "cera":
+            if not busqueda.isdigit():
+                messages.error(request, '¡El valor de Dilución en Cera, debe ser un número entero!')
+                return redirect('listafungicida')
+            lista = Dosificacion.objects.filter(cer_dos__exact=busqueda)
+        elif campo == "trabajador":
+            if busqueda.replace('.','',1).isdigit():
+                messages.error(request, '¡El nombre de trabajador no puede ser un número!')
+                return redirect('listafungicida')
+            lista = Dosificacion.objects.filter(
+                    Q(trabajador_id__nom_tra__icontains=busqueda) | 
+                    Q(trabajador_id__app_tra__icontains=busqueda)
+                ).distinct()
+        elif campo == "observacion":
+                lista = Dosificacion.objects.filter(obs_dos__icontains=busqueda)
+        else:
+            lista = Dosificacion.objects.all().order_by('-id')
+            messages.error(request, '¡Campo de búsqueda inexistente!')
+            return redirect('listafungicida')
+    
+
+    lista_formato = []
+    for grupo in lista:
+        lista_formato.append({
+            'id': grupo.id,
+            'fecha': grupo.dia_id.dia_dia.strftime('%Y-%m-%d'),
+            'hora': grupo.hor_dos,
+            'producto': grupo.fungicidas_id.nom_fun.title(),
+            'peso_inicial': grupo.pei_dos,
+            'peso_final': grupo.pef_dos,
+            'cc_producto': grupo.ccp_dos,
+            'linea': grupo.lineas_id.num_lin,
+            'especie': grupo.especies_id.nom_esp.capitalize(),
+            'variedad': grupo.variedad_id.nom_var.title(),
+            'agua': grupo.agu_dos,
+            'cera': grupo.cer_dos,
+            'trabajador': f"{grupo.trabajador_id.nom_tra.capitalize()} {grupo.trabajador_id.app_tra.capitalize()}",
+            'observacion': grupo.obs_dos,
+        })
+
+    paginator = Paginator(lista_formato, 15)
+    pagina = request.GET.get("page") or 1
+    listas = paginator.get_page(pagina)
+    pagina_actual = int(pagina)
+    paginas = range(1, listas.paginator.num_pages + 1)
+
+    datos = {
+        'listas': listas,
+        'paginas': paginas,
+        'pagina_actual': pagina_actual,
+    }
+    return render(request, 'fungicidas/base/listafungicida.html', datos)
