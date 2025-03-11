321a497ef5a185cf66547968cb0c869f492760d7
Matias Benavides <matias.benavides.rojas@gmail.com>
2025-03-11 12:12:45 -0300

    feat(views): Funcionalidad Listado y busqueda dinámica
    - 'mostrarListaProducto': Hace comprovaciones en form de busqueda, basandose en el campo
        - formatea los datos de GrupoProducto en una lista
        - Manda en un diccionario: Paginador, cantidad de registros a mostrar, la lista formateada, pagina actual
    - Se agregan futuras funciones al path y se deja en pass en views.py
    - Se crea 'listaproducto.html' como listado de reportes de app
    - Se crea paginacion propia para listaproducto.html llamado 'paginacionproductolista.html'

--- Productos/urls.py
+++ Productos/urls.py
@@ -5,4 +5,9 @@ urlpatterns = [
     path('producto/<int:linea_id>', views.mostrarProducto, name="producto"),
     path('registrar_producto/<int:linea_id>', views.registrarProducto, name="registrarproducto"),
 
+    path('archivos_producto/', views.mostrarListaProducto, name="listaproducto"),
+    path('visualizar_producto/<int:grupo_id>/', views.visualizarProducto, name="visualizarproducto"),
+    path('actualizar_producto/<int:grupo_id>/', views.actualizarProducto, name="actualizarproducto"),
+    path('eliminar_producto/<int:grupo_id>/', views.eliminarProducto, name="eliminarproducto"),
+    path('pdf_producto/<int:grupo_id>/', views.PDFProducto, name="pdfproducto")
 ]
